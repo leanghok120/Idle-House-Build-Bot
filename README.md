d93da4df5d49f216af86eac4e6641ffafc2985a6
Leanghok120 <leanghokoeng5@gmail.com>
2023-03-05 12:10:45 +0700

    Added the files

---
+++ README.md
@@ -0,0 +1,34 @@
+# Idle House Build game bot
+
+This is a simple bot that plays Idle House Build for you. It uses Python Gui Automation to automate the game and collect resources.
+
+## Features
+
+- Automatically clicks on buttons, upgrades.
+- Saves and loads the game progress periodically.
+- Customizable settings and preferences.
+
+## Requirements
+
+- Python 3.6 or higher
+
+## Installation
+
+1. Clone this repository or download the zip file.
+2. Install the required packages using pip:
+
+pip install -r requirements.txt
+
+## Usage
+
+1. Go to https://www.crazygames.com/game/idle-house-build
+
+2. Run the bot.py file using Python:
+
+python bot.py
+
+3. Enjoy watching the bot play for you!
+
+## Disclaimer
+
+This bot is for educational purposes only and is not intended to violate any terms of service or rules of the games. Use it at your own risk and responsibility.
---
+++ bot.py
@@ -0,0 +1,89 @@
+import pyautogui
+import keyboard
+import time
+import win32api, win32con
+
+"""
+A python bot that plays Idle Build House
+This is made by Leanghok
+
+My socials
+{
+    GitHub: https://github.com/leanghok120
+    Discord: Astrox#2678
+}
+
+Cords
+{
+    The cordinates of the house: X:  660 Y:  559 RGB: (222, 121,  89)
+    The cordinates of the collect button: X: 1254 Y:  557 RGB: (255, 250, 190)
+    The cordinates of the House size upgrade button: X: 1386 Y:  383 RGB: ( 30, 180,  30)
+    The cordinates of the Bricks per click upgrade button: X: 1387 Y:  308 RGB: ( 30, 180,  30)
+
+}
+"""
+
+bot = False
+
+# The main bot loop
+def main():
+    print(
+        """                                                                                                                                  
+,--.   ,--.,--.           ,--.  ,--.                                 ,-----.          ,--.,--.   ,--.    ,-----.           ,--.   
+|  | ,-|  ||  | ,---.     |  '--'  | ,---. ,--.,--. ,---.  ,---.     |  |) /_ ,--.,--.`--'|  | ,-|  |    |  |) /_  ,---. ,-'  '-. 
+|  |' .-. ||  || .-. :    |  .--.  || .-. ||  ||  |(  .-' | .-. :    |  .-.  \|  ||  |,--.|  |' .-. |    |  .-.  \| .-. |'-.  .-' 
+|  |\ `-' ||  |\   --.    |  |  |  |' '-' ''  ''  '.-'  `)\   --.    |  '--' /'  ''  '|  ||  |\ `-' |    |  '--' /' '-' '  |  |   
+`--' `---' `--' `----'    `--'  `--' `---'  `----' `----'  `----'    `------'  `----' `--'`--' `---'     `------'  `---'   `--'   
+                                                                                                                                  """
+    )
+    print("\n(+) Bot: Enabled\nPress shift+r to toggle bot\nPress Esc to exit\n")
+
+    while not keyboard.is_pressed("Esc"):
+        if bot:
+            autoBuild()
+            autoUpgrade()
+
+    print("(+) Bot: Disabled\n")
+
+
+# This function will turn on the bot when you press shift+r
+def toggle_bot():
+    global bot
+    bot = not bot
+
+
+keyboard.add_hotkey("shift+r", toggle_bot)
+
+# This function will click on the house and press enter when the collect button appears
+def autoBuild():
+    click(660, 559)
+    if pyautogui.pixelMatchesColor(1254, 557, (255, 250, 190)):
+        keyboard.press_and_release("enter")
+        time.sleep(0.5)
+
+
+# This function will click on the upgrade
+def autoUpgrade():
+    if pyautogui.pixelMatchesColor(1386, 383, (30, 180, 30)):
+        click(1386, 383)
+        time.sleep(0.5)
+    if pyautogui.pixelMatchesColor(1387, 308, (30, 180, 30)):
+        click(1387, 308)
+        time.sleep(0.5)
+    if pyautogui.pixelMatchesColor(
+        1386, 383, (30, 180, 30)
+    ) and pyautogui.pixelMatchesColor(1387, 308, (30, 180, 30)):
+        click(1387, 308)
+        time.sleep(0.5)
+
+
+# Just a normal click function
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    time.sleep(0.00001)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+
+
+if __name__ == "__main__":
+    main()
